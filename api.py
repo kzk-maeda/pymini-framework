d714cb03ffe5cc675e5b8cdccad811eae5ec9c02
kzk-maeda <kzk.maeda0711@gmail.com>
2021-06-02 10:06:13 +0900

    basic route handler

--- api.py
+++ api.py
@@ -3,16 +3,32 @@ from typing import Callable, Dict
 from webob import Request, Response
 
 class API:
+  def __init__(self) -> None:
+      self.routes = {}
+
+
   def __call__(self, environ: Dict, start_response: Callable) -> Response:
     request = Request(environ)
     response = self.handle_request(request)
 
     return response(environ, start_response)
-  
+
+
+  def route(self, path: str) -> Callable:
+    def wrapper(handler):
+      self.routes[path] = handler
+      return handler
+    
+    return wrapper
+
+
   def handle_request(self, request: Request) -> Response:
     user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
 
     response = Response()
-    response.text = f'Hello, my friend with this user-agent: {user_agent}'
+    for path, handler in self.routes.items():
+      if path == request.path:
+        handler(request, response)
+        return response
 
     return response
--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 # app.py
-
+from webob import Request, Response
 from api import API
 
 app = API()
+
+
+@app.route('/home')
+def home(request: Request, response: Response) -> None:
+  response.text = 'Hello from the Home page'
+
+
+@app.route('/about')
+def about(request: Request, response: Response) -> None:
+  response.text = 'Hello from the About page'
